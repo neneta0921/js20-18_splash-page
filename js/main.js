2ab074c3b608ea5332cfd2053f3934c513857d5f
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-02-03 15:34:09 +0900

    remove comment

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
 const { body } = document;
 
-// let count = [];
-
 function changeBackground(number) {
-
-  // for (let i = 0; i < 1000; i++) {
-  //   document.body.appendChild(document.createElement('div'));
-  // }
-  // count.push(new Array(100000).join("count"));
-
   const selectedClass = `background-${number}`;
   // If body hasn't class
   if (!body.className) {
     body.classList.add(selectedClass);
   } else {
     // If body has class, check previous one
-    const previousNumber = body.className.slice(-1)
+    const previousNumber = body.className.slice(-1);
     const previousClass = `background-${previousNumber}`;
     if (number === previousNumber) {
-        body.classList.remove(selectedClass);
-      } else {
-        body.classList.remove(previousClass);
-        body.classList.add(selectedClass);
+      body.classList.remove(selectedClass);
+    } else {
+      body.classList.remove(previousClass);
+      body.classList.add(selectedClass);
     }
   }
 }
