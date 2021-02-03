120e9197d9a2283e31afcfb6e12315e5f63b30d7
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-02-03 15:46:01 +0900

    added Main class

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-const { body } = document;
+document.addEventListener('DOMContentLoaded', () => {
+  new Main();
+});
 
-function changeBackground(number) {
-  const selectedClass = `background-${number}`;
-  // If body hasn't class
-  if (!body.className) {
-    body.classList.add(selectedClass);
-  } else {
+class Main {
+  constructor() {
+    this._init();
+  }
+
+  _init() {
+    this._addEvent();
+  }
+
+  _checkPrevious(body) {
     // If body has class, check previous one
     const previousNumber = body.className.slice(-1);
     const previousClass = `background-${previousNumber}`;
@@ -15,5 +21,27 @@ function changeBackground(number) {
       body.classList.remove(previousClass);
       body.classList.add(selectedClass);
     }
+  }
+
+  _changeBackground(number) {
+    const { body } = document;
+    const selectedClass = `background-${number}`;
+
+    // If body hasn't class
+    if (!body.className) {
+      body.classList.add(selectedClass);
+    } else {
+      this._checkPrevious(body);
+    }
+  }
+
+  _addEvent() {
+    const background1 = document.querySelector('.background-1');
+    const background2 = document.querySelector('.background-2');
+    const background3 = document.querySelector('.background-3');
+
+    background1.addEventListener('click', () => this._changeBackground(1));
+    background2.addEventListener('click', () => this._changeBackground(2));
+    background3.addEventListener('click', () => this._changeBackground(3));
   }
 }
